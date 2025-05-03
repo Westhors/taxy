b86c7d50d6a4b691c75c36705c7937c826ea4ffe
YSamer <ysamer2525@gmail.com>
2025-05-03 06:44:25 +0300

    Init Reverb (RealTime)

---
+++ app/Console/Commands/TestOrder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Events\NewOrderRequest;
+use App\Models\Order;
+use Illuminate\Console\Command;
+
+class TestOrder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:order {--id=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $id =  $this->option('id');
+        broadcast(new NewOrderRequest(null));
+    }
+}
